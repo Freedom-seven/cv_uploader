d6214ed689ced0f870c00c6a9eae35e7ecd8d1c3
FREEDOM <freewhynane62@gmail.com>
2024-03-22 04:49:47 +0000

    add colors constants

---
+++ constants/Colors.js
@@ -0,0 +1,11 @@
+// constants/Colors.js
+
+export default {
+  PRIMARY_COLOR: "#1E90FF", // Dodger Blue
+  SECONDARY_COLOR: "#F5F5F5", // White
+  BACKGROUND_COLOR: "#FFF", // White Smoke
+  TEXT_COLOR: "#333333", // Very Dark Gray
+  ERROR_COLOR: "#FF0000", // Red
+  SUCCESS_COLOR: "#008000", // Green
+  WARNING_COLOR: "#FFA500", // Orange
+};
